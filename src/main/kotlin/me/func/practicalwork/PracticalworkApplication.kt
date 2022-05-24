bdd1379605add7db3811f52d856a593925eac0bb
Царюк Артём <42806772+funcid@users.noreply.github.com>
2022-05-24 17:38:12 +0300

    Добавлен шаблон

---
+++ src/main/kotlin/me/func/practicalwork/PracticalworkApplication.kt
@@ -0,0 +1,11 @@
+package me.func.practicalwork
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PracticalworkApplication
+
+fun main(args: Array<String>) {
+	runApplication<PracticalworkApplication>(*args)
+}
---
+++ src/test/kotlin/me/func/practicalwork/PracticalworkApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.func.practicalwork
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PracticalworkApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
